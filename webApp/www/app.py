f4818fc4f6efd43d854e0531b7ee58fc334b0e47
simba <18721784110@163.com>
2024-02-26 18:20:58 +0800

    2024-2-26:18-20

--- webApp/www/app.py
+++ webApp/www/app.py
@@ -3,11 +3,17 @@ logging.basicConfig(level=logging.INFO)
 
 import asyncio,os,json,time
 from datetime import datetime
+
+
 from aiohttp import web
 from jinja2 import Environment,FileSystemLoader
+
+from config import configs
+
 import orm
 from coroweb import add_routes,add_static
 
+from handlers import cookie2user,COOKIE_NAME
 
 def init_jinja2(app,**kw):
   logging.info('init jinja2...')
@@ -31,11 +37,27 @@ def init_jinja2(app,**kw):
       env.filters[name] = f
   app['__templating__'] = env
 
+
 async def logger_factory(app,handler):
   async def logger(request):
     logging.info('Request: %s %s' % (request.method,request.path))
     return (await handler(request))
   return logger
+
+async def auth_factory(app,handler):
+  async def auth(request):
+    logging.info('check user: %s %s' % (request.method,request.path))
+    request.__user__=None
+    cookie_str = request.cookies.get(COOKIE_NAME)
+    if cookie_str:
+      user = await cookie2user(cookie_str)
+      if user:
+        logging.info('set currrent user: %s' % user.email)
+        request.__user__ = user
+      if request.path.startswith('/manage/') and (request.__user__ is not None or not request.__user__.admin):
+        return web.HTTPFound('/signin')
+      return (await handler(request))
+    return auth
 
 
 async def data_factory(app,handler):
@@ -111,7 +133,7 @@ def index(request):
 
 async def init(loop):
   await orm.create_pool(loop=loop,host='127.0.0.1',port=3306,user='root',password=123123,db='python')
-  app = web.Application(loop=loop,middlewares=[logger_factory,response_factory])
+  app = web.Application(loop=loop,middlewares=[logger_factory,auth_factory,response_factory])
   init_jinja2(app,filters=dict(datetime=datetime_filter))
   add_routes(app,'handlers')
   add_static(app)
